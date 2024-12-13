95f6130876641d7968f988d1b25a7aa6ae1e6e5a
bazzilic <bazzilic@gmail.com>
2024-12-13 20:22:14 +0400

    second

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deployment from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+
+export class WebsiteStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create S3 Bucket for static website hosting
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Be cautious in production
+    });
+
+    // Deploy static files from frontend build directory
+    new s3deployment.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deployment.Source.asset('../frontend/public')],
+      destinationBucket: websiteBucket,
+    });
+
+    // Output the website URL
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: websiteBucket.bucketWebsiteUrl
+    });
+  }
+}
